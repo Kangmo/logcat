755cd9dfe906bc8822aeb623f6a1dd0a1d61a9b3
kangmo <kangmo@korbit.co.kr>
2014-09-29 04:04:17 +0900

    Treat IP address as noise. Otherwise IP address can be extracted as a feature if the same IP address comes over and over.

--- main.scala
+++ main.scala
@@ -14,6 +14,11 @@ case class Token(val value : String, var count : Int = 0, var isFeature : Boolea
 class TokensCategory(val tokens: Array[Token]) {
 }
 
+object Helper {
+	def isIpAddress(string : String) =
+		"""(\d)+.(\d)+.(\d)+.(\d)""".r.findFirstMatchIn(string).isDefined
+	def isNoise(string : String) = isIpAddress(string)
+}
 object LogParser {
 	def parse(logLine:String) = {
 		val tokens = logLine.split("\t")
@@ -34,9 +39,12 @@ object LogCategorizer {
 	def main(args: Array[String]) {
 		val files = new java.io.File("../thelog").listFiles()
 
-		val tokenCountMap = scala.collection.mutable.Map[Int, scala.collection.mutable.ArrayBuffer[TokensCategory]]()
-
 		files.map { file =>
+			println("****************************************")
+			println(file.toString)
+			println("****************************************")
+			val tokenCountMap = scala.collection.mutable.Map[Int, scala.collection.mutable.ArrayBuffer[TokensCategory]]()
+
 			var line = ""
 		  	for(line <- Source.fromFile(file).getLines()) {
 		  		val (headers:Array[String], log:String) = LogParser.parse(line)
@@ -76,64 +84,70 @@ object LogCategorizer {
 */
 		  		//learner.add(headers, logTokens, log)
 		  	}
-		}
 
-		// count tokens at the same position
-		tokenCountMap.map {
-			case (tokenCount, catArray) => {
-//				println("token length = " + tokenCount)
-//				println("category count = " + catArray.length)
+			// count tokens at the same position
+			tokenCountMap.map {
+				case (tokenCount, catArray) => {
+	//				println("token length = " + tokenCount)
+	//				println("category count = " + catArray.length)
 
-				// For each token at poistion, set token count grouped by token 
-				for (i <- 0 until tokenCount) {
-//					println("token indx = " + i)
+					// For each token at poistion, set token count grouped by token 
+					for (i <- 0 until tokenCount) {
+	//					println("token indx = " + i)
 
-					val sortedTokens = catArray.map{
-						_.tokens(i)
-					}.sortBy(_.value)
+						val sortedTokens = catArray.map{
+							_.tokens(i)
+						}.sortBy(_.value)
 
-					val groups = sortedTokens.groupBy(_.value)
-					groups.map {
-						case (group, tokens) =>
-							tokens.foreach {
-								t => t.count = tokens.length
-							}
+						val groups = sortedTokens.groupBy(_.value)
+						groups.map {
+							case (group, tokens) =>
+								tokens.foreach {
+									t => t.count = tokens.length
+								}
+						}
 					}
-				}
 
-				// For each category, get the token with maximum number of occurence.
-				// Set all tokens having the same occurrence as a "feature" token.
-				for (cat <- catArray) {
-					val tokenWithMaxCount = cat.tokens.max(Ordering.by((_:Token).count))
-					cat.tokens.foreach { t =>
-						if (t.count == tokenWithMaxCount.count)
-							t.isFeature = true;
+					// For each category, get the token with maximum number of occurence.
+					// Set all tokens having the same occurrence as a "feature" token.
+					for (cat <- catArray) {
+						val featureCandidates = cat.tokens.
+								filter{t=> !Helper.isNoise(t.value)}
+						if ( ! featureCandidates.isEmpty ) {
+							val tokenWithMaxCount = 
+								 // Get rid of noise such as IP address
+								featureCandidates.max(Ordering.by((_:Token).count))
+							featureCandidates.foreach { t =>
+								if (t.count == tokenWithMaxCount.count)
+									t.isFeature = true;
+							}
+						}
 					}
 				}
 			}
-		}
 
-		// Treat a token a noise token if it is not a feature token.
-		// Group categories by feature tokens
-		tokenCountMap.map {
-			case (tokenCount, catArray) => {
-				println("=================================================================================")
-				println(s"Log Category with token length = ${tokenCount}. Total count = ${catArray.length}"  )
-				println("=================================================================================")
+			// Treat a token a noise token if it is not a feature token.
+			// Group categories by feature tokens
+			tokenCountMap.map {
+				case (tokenCount, catArray) => {
+					println("=================================================================================")
+					println(s"Log Category with token length = ${tokenCount}. Total count = ${catArray.length}"  )
+					println("=================================================================================")
 
-				val groups = catArray.map { cat =>
-					val log = cat.tokens.map { t =>
-						if ( t.isFeature ) t.value else "_"
-					}.mkString(",")
-					log
-				}.groupBy(_.toString)
+					val groups = catArray.map { cat =>
+						val log = cat.tokens.map { t =>
+							if ( t.isFeature ) t.value else "_"
+						}.mkString(",")
+						log
+					}.groupBy(_.toString)
 
-				groups.map {
-					case (group, groupArray) =>
-						println( s"[${groupArray.length}]${group}")
-				}	
-			} 
+					groups.map {
+						case (group, groupArray) =>
+							println( s"[${groupArray.length}]${group}")
+					}	
+				} 
 
+			}		  	
 		}
 	}
 }
